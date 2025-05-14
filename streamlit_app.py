44fcb263bea81fce25758f64cc3703c4048c24dc
Rahul Bouri <102468432+arcrahul@users.noreply.github.com>
2025-05-14 20:04:36 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+st.title("Gold Price Prediction")
+ticker = 'GC=F'  # Gold Futures
+data = yf.download(ticker, period='1y')
+
+st.line_chart(data['Close'])
